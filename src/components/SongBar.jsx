50e6f31165d14e91271235aa6be86771384c027f
Shaji-Mohammed <mshajisufiyan@gmail.com>
2023-07-31 17:24:03 -0300

    created top charts and top artists

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -9,10 +9,10 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
         className="w-20 h-20 rounded-lg"
-        src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song.images?.coverart}
+        src={artistId ? song.images?.coverart.url.replace('{w}', '125').replace('{h}', '125') : song.images?.coverart}
         alt={song.title}
       />
-      <div className="flex-1 flex flex-col justify-center mx-3">
+      <div className="flex-1 flex flex-col justify-center mx-3 ">
         {!artistId ? (
           <Link to={`/songs/${song.key}`}>
             <p className="text-xl font-bold text-white">
